60bff4f40d684cab5a5b5b34ba57ae66126ef964
dragonfeifei <f.song8985@gmail.com>
2018-08-12 11:23:30 -0700

    add another implementation

--- src/antifraoud.py
+++ src/antifraoud.py
@@ -1,11 +1,45 @@
 import argparse
 import csv
-import networkx as nx
+from collections import deque, defaultdict
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+
+def shortest_path_length(graph, node1, node2):
+    if node1 == node2:
+        return 0
+    if (node1 not in graph.nodes) or \
+       (node2 not in graph.nodes):
+       return -1
+    queue = deque()
+    visited = defaultdict()
+    queue.appendleft(node1)
+    visited[node1] = 0
+    while len(queue) > 0:
+        current = queue.pop()
+        for i in graph.edges[current]:
+            if i in visited:
+                continue
+            visited[i] = visited[current]+1
+            if i == node2:
+                return visited[i]
+            queue.appendleft(i)
+    return -1
+
 
 class Verifier:
     def __init__(self, outputRepo):
         self.outputRepo = outputRepo
-        self.graph = nx.Graph()
+        self.graph = Graph()
 
     def pre_process(self, batchFile):
         with open(batchFile, 'r') as csvfile:
@@ -13,7 +47,8 @@ class Verifier:
             for row in reader:
                 node1 = row['id1']
                 node2 = row['id2']
-                self.graph.add_nodes_from([node1, node2])
+                self.graph.add_node(node1)
+                self.graph.add_node(node2)
                 self.graph.add_edge(node1, node2)
 
     def process(self, streamFile):
@@ -25,28 +60,23 @@ class Verifier:
             for row in reader:
                 node1 = row['id1']
                 node2 = row['id2']
-                if node1 in self.graph and \
-                   node2 in self.graph and \
-                   nx.has_path(self.graph, node1, node2):
-                    distance = nx.shortest_path_length(self.graph, node1, node2)
-                    if distance == 1:
-                        out1.write('trusted\n')
-                        out2.write('trusted\n')
-                        out3.write('trusted\n')
-                        continue
-                    elif distance == 2:
-                        out1.write('unverified\n')
-                        out2.write('trusted\n')
-                        out3.write('trusted\n')
-                        continue
-                    elif distance < 5:
-                        out1.write('unverified\n')
-                        out2.write('unverified\n')
-                        out3.write('trusted\n')
-                        continue
-                out1.write('unverified\n')
-                out2.write('unverified\n')
-                out3.write('unverified\n')
+                distance = shortest_path_length(self.graph, node1, node2)
+                if distance == 1:
+                    out1.write('trusted\n')
+                    out2.write('trusted\n')
+                    out3.write('trusted\n')
+                elif distance == 2:
+                    out1.write('unverified\n')
+                    out2.write('trusted\n')
+                    out3.write('trusted\n')
+                elif distance < 5 and distance > 2:
+                    out1.write('unverified\n')
+                    out2.write('unverified\n')
+                    out3.write('trusted\n')
+                else:
+                    out1.write('unverified\n')
+                    out2.write('unverified\n')
+                    out3.write('unverified\n')
 
 
 def main(batchFile, streamFile, outputRepo):
